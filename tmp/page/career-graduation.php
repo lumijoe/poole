2d42931f372f9c6150dbfd2f12989a21cf26bb17
lumijoe <spacedesign63@gmail.com>
2024-07-31 16:10:11 +0900

    test

--- tmp/page/career-graduation.php
+++ tmp/page/career-graduation.php
@@ -18,24 +18,20 @@
       <h1 class="c-secttl is-red"><?php the_title(); ?></h1>
       <div class="c-flex-col2 mt50">
         <div>
-          <h2 class="c-secttl-box">進路結果</h2>
-          <img class="mt30" src="<?php echo $imagePath ?>career/graduation/result01.jpg" alt="大学進学率94.9%" width="555" height="352">
+          <h2 class="c-secttl-box">進路状況</h2>
+          <img class="mt30" src="<?php echo $imagePath ?>career/graduation/img01_shinrojokyo.jpg" alt="大学進学率91.9%、国公立を含む難関有名大学に72.3%が進学" width="1200" height="auto" style="max-width: 1200px;">
         </div>
-        <div>
-          <h2 class="c-secttl-box">4年制大学進学者内訳</h2>
-          <img class="mt10" src="<?php echo $imagePath ?>career/graduation/result02.jpg" alt="国公立を含む難関大学に71.6%が進学" width="518" height="469">
-        </div>
-      </div><!-- /.c-flex-col2 -->
+      </div>
     </section>
 
     <section class="mt70">
       <h2 class="c-secttl-box">合格大学一覧</h2>
-      <img class="mt30" src="<?php echo $imagePath ?>career/graduation/list01.jpg" alt="国公立10名、関関同立31名、産近甲龍38名、主要女子大77名、首都圏25名、海外1名、医歯薬看護系73名、食物栄養系15名、芸術系14名" width="1200" height="620">
+      <img class="mt30" src="<?php echo $imagePath ?>career/graduation/img02_daigaku.jpg" alt="国公立8名、関関同立24名、産近甲龍56名、主要女子大71名、首都圏18名、医歯薬看護系74名、食物栄養系12名、芸術系12名" width="1200" height="620">
     </section>
 
     <section class="mt70">
       <h2 class="c-secttl-box">指定校推薦一覧</h2>
-      <img class="mt30" src="<?php echo $imagePath ?>career/graduation/list02.jpg" alt="指定校推薦の対象となるコース在籍者190名に対し、147大学、800名以上の指定校枠が付与されています" width="1200" height="649">
+      <img class="mt30" src="<?php echo $imagePath ?>career/graduation/img03_suisen.jpg" alt="指定校推薦の対象となるコース在籍者160名に対し、150大学、800名以上の指定校枠が付与されています" width="1200" height="649">
     </section>
 
     <div class="c-btnList mt100">
