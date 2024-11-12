13c71ce66dc39c9e3eceb5b2e81feef6c895df3b
小城 <kojolumi@gamil.com>
2024-11-12 14:15:20 +0900

    test

---
+++ tmp/page/for_graduate-hatachi_form.php
@@ -0,0 +1,23 @@
+<div class="c-pagettl">
+  <img src="<?php echo $imagePath ?>for_graduate/mv.jpg" alt="">
+  <h1 class="bg-red c-pagettl__ttl">
+    卒業生の方へ
+  </h1>
+</div><!-- /.c-pagettl -->
+<!-- ▼ breadcrumb =============================== -->
+<?php custom_breadcrumb(); ?>
+<!-- ▲ breadcrumb =============================== -->
+
+<div class="p-contact_header">
+	<h2 class="c-secttl-red">はたちの感謝礼拝お申込フォーム</h2>
+	<p class="mb30 mb30-30">下記フォームに必要事項を入力して「確認」ボタンを押してください。</p>
+</div>
+
+<div class="content p-contact">  
+  
+  <div class="p-contact__inner">  	
+    <?php the_field('contact');?>
+
+
+  </div><!-- /.p-contact__inner -->
+</div><!-- /.content -->
---
+++ tmp/page/for_graduate-hatachi_form_complete.php
@@ -0,0 +1,28 @@
+<div class="c-pagettl">
+  <img src="<?php echo $imagePath ?>for_graduate/mv.jpg" alt="">
+  <h1 class="bg-red c-pagettl__ttl">
+    卒業生の方へ
+  </h1>
+</div><!-- /.c-pagettl -->
+<!-- ▼ breadcrumb =============================== -->
+<?php custom_breadcrumb(); ?>
+<!-- ▲ breadcrumb =============================== -->
+
+
+
+<div class="p-contact_header">
+	<h2 class="c-secttl-red">はたちの感謝礼拝お申込フォーム</h2>
+</div>
+
+<div class="content p-contact">
+
+  <div class="p-contact__inner">
+    <p class="mb30">はたちの感謝礼拝にお申し込みいただき、ありがとうございました。<br>
+      担当者よりご連絡いたしますので、しばらくお待ちください。</p>
+    <div class="u-center mt40">
+      <a href="<?php echo esc_url(home_url('/')); ?>" class="c-btn">トップページへ</a>
+    </div><!-- /.u-center -->
+
+
+  </div><!-- /.p-contact__inner -->
+</div><!-- /.content -->
